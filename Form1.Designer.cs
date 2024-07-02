316b920263968cb547d347575be24ce8712cf52d
Stryzhakov Bogdan <bogdan24086@gmail.com>
2024-07-02 21:51:48 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIpAddress = new System.Windows.Forms.TextBox();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP - Address";
+            // 
+            // txtIpAddress
+            // 
+            this.txtIpAddress.Location = new System.Drawing.Point(12, 182);
+            this.txtIpAddress.Name = "txtIpAddress";
+            this.txtIpAddress.Size = new System.Drawing.Size(165, 20);
+            this.txtIpAddress.TabIndex = 1;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 287);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(165, 23);
+            this.progressBar.TabIndex = 2;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(15, 232);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 3;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(225, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Result";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lstResult
+            // 
+            this.lstResult.FormattingEnabled = true;
+            this.lstResult.Location = new System.Drawing.Point(228, 145);
+            this.lstResult.Name = "lstResult";
+            this.lstResult.Size = new System.Drawing.Size(414, 199);
+            this.lstResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.lstResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnScan);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.txtIpAddress);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIpAddress;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnScan_Click(object sender, EventArgs e)
+        {
+            string ipAddress = txtIpAddress.Text;
+            lstResult.Items.Clear();
+            progressBar.Value = 0;
+            progressBar.Maximum = 65535; // Установить максимальное значение прогресса
+
+            await Task.Run(() => ScanPorts(ipAddress));
+        }
+
+        private async Task ScanPorts(string ipAddress)
+        {
+            int openPortsCount = 0;
+            int totalPorts = 65535;
+            ParallelOptions parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = 100 };
+
+            Parallel.For(1, totalPorts + 1, parallelOptions, async port =>
+            {
+                bool isOpen = await IsPortOpen(ipAddress, port);
+                if (isOpen)
+                {
+                    Invoke(new Action(() => lstResult.Items.Add($"Port {port} is open.")));
+                    openPortsCount++;
+                }
+
+                // Обновляем прогрессбар
+                Invoke(new Action(() => progressBar.Value++));
+            });
+        }
+
+        private async Task<bool> IsPortOpen(string ipAddress, int port)
+        {
+            try
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    var connectTask = client.ConnectAsync(ipAddress, port);
+                    var timeoutTask = Task.Delay(200); // 200 ms timeout
+                    var completedTask = await Task.WhenAny(connectTask, timeoutTask);
+
+                    if (completedTask == connectTask)
+                    {
+                        await connectTask; // re-throw exception if connectTask faulted
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                // Ignored
+            }
+            return false;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
